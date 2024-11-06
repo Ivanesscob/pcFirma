f0f2bc46510327e0573cbb5ea1d5c6ff93c571f5
Ivanesscob <Ivanesscob@gmail.com>
2024-11-06 19:53:43 +0300

    add russian lg support

--- ToPdf.cs
+++ ToPdf.cs
@@ -18,28 +18,30 @@ namespace PcFirma
             PdfWriter writer = PdfWriter.GetInstance(pdfDoc, new FileStream(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), nameFile), FileMode.Create));
             pdfDoc.Open();
 
+            
+            string fontPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "ARIAL.TTF");
+            BaseFont baseFont = BaseFont.CreateFont(fontPath, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            Font font = new Font(baseFont, 12); 
 
             PdfPTable pdfTable = new PdfPTable(data.ColumnCount);
             pdfTable.WidthPercentage = 100;
 
-
             foreach (DataGridViewColumn column in data.Columns)
             {
-                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText, font));
                 cell.BackgroundColor = BaseColor.LIGHT_GRAY;
                 pdfTable.AddCell(cell);
             }
-
 
             foreach (DataGridViewRow row in data.Rows)
             {
                 foreach (DataGridViewCell cell in row.Cells)
                 {
-                    pdfTable.AddCell(cell.Value?.ToString() ?? string.Empty);
+                    pdfTable.AddCell(new Phrase(cell.Value?.ToString() ?? string.Empty, font)); 
                 }
             }
 
-            MessageBox.Show("File created");
+            MessageBox.Show("File created!");
             pdfDoc.Add(pdfTable);
             pdfDoc.Close();
 
